95624451b02f0f6ab81a46c2c870a83cdcb399b1
calixtollucas <lucaslucenacalixto1@gmail.com>
2025-06-01 01:09:20 +0000

    initial commit

---
+++ src/main/java/dev/ruka/FIPETableConsult/FipeTableConsultApplication.java
@@ -0,0 +1,13 @@
+package dev.ruka.FIPETableConsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeTableConsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeTableConsultApplication.class, args);
+	}
+
+}
